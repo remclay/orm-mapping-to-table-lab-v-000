d281dc301d9b8a943c54e654db8644aa9d11a1a0
Remmy Clay <remmy.lauren@gmail.com>
2018-01-31 19:13:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,13 +22,7 @@ class Student
   end
 
   def self.drop_table
-  #  sql = <<-SQL
-  #    DROP TABLE students;
-  #    SQL
-
-  #  DB[:conn].execute(sql)
-
-  DB[:conn].execute("DROP TABLE students")
+    DB[:conn].execute("DROP TABLE students")
   end
 
   def save
